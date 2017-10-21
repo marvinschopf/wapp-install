cd3f062f21ccd735456b34f2c841c475f52adc41
MarvMan <24806960+MagicMarvMan@users.noreply.github.com>
2017-10-21 14:31:57 +0100

    Create wapp_interactive.py

---
+++ wapp_interactive.py
@@ -0,0 +1,10 @@
+import os
+import platform
+
+class WappInstaller:
+    def __init__(self):
+        # DO SOMETHING
+    def start(self):
+        # DO SOMETHING
+    def install_app(app,self):
+        # DO SOMETHING
